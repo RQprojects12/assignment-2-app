1cc7f5630084d2fa71fae4df5feffdca353fc3df
Rabia <qrabia798@gmail.com>
2025-04-27 23:49:58 +0100

    for testing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import datetime
 import logging
 
 # Define the application version and fetch the hostname from environment variables
-APP_VERSION = "1.0.2"
+APP_VERSION = "1.0.2 rabia"
 HOSTNAME = os.environ.get("HOSTNAME", "unknown")
 #and what more
 
